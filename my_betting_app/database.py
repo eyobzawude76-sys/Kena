db843d05f73d0e0aba46f32dce3de1d335bd4c9c
EYOB <eyobzawude76@gmail.com>
2026-06-20 20:49:26 +0300

    mx

--- my_betting_app/database.py
+++ my_betting_app/database.py
@@ -1,6 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
 # MongoDB connection
-MONGO_URL = "mongodb+srv://eyobzawude76_db_user:12357ab%40%23@cluster0.uo74prq.mongodb.net/greyhound_betting_db?retryWrites=true&w=majority"
+MONGO_URL = "mongodb+srv://kena_user:kenapassword123@cluster0.uo74prq.mongodb.net/greyhound_betting_db?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.greyhound_betting_db
